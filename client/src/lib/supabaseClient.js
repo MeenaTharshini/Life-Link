6c602886c32300a3ce28bdfb9dc4ae8aa6fbc22a
Meena Tharshini I <meenatharshiniiyappan@gmail.com>
2026-07-27 17:40:41 +0530

    Updated

--- client/src/lib/supabaseClient.js
+++ client/src/lib/supabaseClient.js
@@ -1,13 +1,10 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error("Missing Supabase environment variables in backend");
+  throw new Error("Missing Supabase environment variables");
 }
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
